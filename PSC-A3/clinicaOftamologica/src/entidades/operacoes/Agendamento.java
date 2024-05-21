641e62dd637f0fd8d23d4d204eac2eaae014d2af
Nair Rosa <contatonairrosa@gmail.com>
2024-05-21 12:33:54 -0300

    Add files via upload

---
+++ PSC-A3/clinicaOftamologica/src/entidades/operacoes/Agendamento.java
@@ -0,0 +1,5 @@
+package entidades.operacoes;
+
+public class Agendamento {
+    
+}
---
+++ PSC-A3/clinicaOftamologica/src/executavel/Principal.java
@@ -0,0 +1,6 @@
+package executavel;
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
